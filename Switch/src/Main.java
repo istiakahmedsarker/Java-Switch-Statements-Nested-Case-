82306b85f9b57814a6c72600c300fb0b28baf06a
Istiak AHmed <gamerestiak99@gmail.com>
2023-12-31 11:01:52 +0600

    completed the sitch

--- Switch/src/Main.java
+++ Switch/src/Main.java
@@ -5,5 +5,11 @@ public class Main {
         Scanner in = new Scanner(System.in);
         String fruit = in.next();
 
+        switch (fruit) {
+            case "Mango" -> System.out.println("King of fruits");
+            case "Apple" -> System.out.println("A sweet red fruit");
+            case "Orange" -> System.out.println("Round fruit");
+            default -> System.out.println("Enter a valid fruit");
+        }
     }
 }
