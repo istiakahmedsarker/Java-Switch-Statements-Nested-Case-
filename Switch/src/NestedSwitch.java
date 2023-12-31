b708bdb2e17a2ab2e86a7cf811d03c4e42a29981
Istiak AHmed <gamerestiak99@gmail.com>
2023-12-31 11:06:36 +0600

    created NestedSwitch

--- Switch/src/NestedSwitch.java
+++ Switch/src/NestedSwitch.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class NestedSwitch {
     public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        int empId= in.nextInt();
+        String department = in.next();
 
+        switch (empId) {
+            case 1 -> System.out.println("Hey Istiak");
+            case 2 -> System.out.println("Hey Mahi");
+            case 3 -> {
+                System.out.println("Hey Lazy");
+                switch (department) {
+                    case "IT" -> System.out.println("Hey IT");
+                    case "Management" -> System.out.println("Hey Manger");
+                    default -> System.out.println("No department");
+                }
+            }
+            default -> System.out.println("No empID");
+        }
     }
 }
